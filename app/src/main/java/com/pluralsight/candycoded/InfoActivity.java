d4518a1d7bb334903546a4dcfc7d2c483e8ab710
abdulrahman <32624418+abdulrahmanbmm@users.noreply.github.com>
2020-09-29 14:45:18 +0100

    Update InfoActivity.java

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -3,6 +3,7 @@ package com.pluralsight.candycoded;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
